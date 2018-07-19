993e0704993cc54f6d8ac408f331ce543a35a43e
jvergara <jvergara@env.ethz.ch>
2018-07-19 11:03:49 +0200

    asdfa

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -1,4 +1,6 @@
 python regrid_run.py
+echo 'end of regrid step'
 python monthly_concatenation.py
+echo 'end of concatenation. Starting with plots'
 python eobs_evaluation_monthly.py
 python eobs_evaluation_seasons.py
