6a5c03cc7e545f3e2b921e86a4311517f69cbea5
jvergara <jvergara@env.ethz.ch>
2019-05-13 10:24:09 +0200

    ready to run the full chain

--- run_evaluation.py
+++ run_evaluation.py
@@ -1,7 +1,7 @@
 import os
 from define_parameters import *
 
-a=os.system('python run_monthly_means.py')
+a=os.system('python run_monthly_means_for_years.py')
 print('Monthly means created')
 
 
@@ -17,6 +17,11 @@ print('Starting with EOBS')
 b=os.system('python regrid_run.py EOBS')
 print('Run regrided to EOBS')
 b=os.system('python evaluate_EOBS.py')
+
+print('Metoswiss evaluation')
+b=os.system('python regrid_run.py meteoswiss_1k')
+print('Run regrided to meteoswiss_1k')
+b=os.system('python evaluate_meteoswiss_1k.py')
 
 #python eobs_evaluation_monthly.py
 #python eobs_evaluation_seasons.py
